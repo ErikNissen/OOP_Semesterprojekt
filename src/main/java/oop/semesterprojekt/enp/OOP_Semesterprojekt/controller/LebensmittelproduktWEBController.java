f342b725a8e4ab7d2b120b6c8473b8430d01c401
Pascal Groß <pascal.gross@student.fh-kiel.de>
2021-06-01 17:12:27 +0200

    Webanwendung erweitert; Webcontroller geschrieben für jede Klasse

---
+++ src/main/java/oop/semesterprojekt/enp/OOP_Semesterprojekt/controller/LebensmittelproduktWEBController.java
@@ -0,0 +1,25 @@
+package oop.semesterprojekt.enp.OOP_Semesterprojekt.controller;
+
+import oop.semesterprojekt.enp.OOP_Semesterprojekt.model.Lebensmittelprodukt;
+import oop.semesterprojekt.enp.OOP_Semesterprojekt.service.LebensmittelproduktService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+public class LebensmittelproduktWEBController {
+
+    @Autowired
+    LebensmittelproduktService lebensmittelproduktService;
+
+    @RequestMapping( value= "/lebensmittel.html")
+    public String alleLebensmittel( Model model ) {
+
+        List<Lebensmittelprodukt> alleLebensmittel = lebensmittelproduktService.getAllLebensmittelprodukt();
+        model.addAttribute("alleLebensmittel", alleLebensmittel);
+        return "lebensmittel.html";
+    }
+}
