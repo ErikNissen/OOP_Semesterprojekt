081a8165ac45c6b10357bbcc7ebcab60ed57a905
Pascal Groß <pascal.gross@student.fh-kiel.de>
2021-06-02 17:41:41 +0200

    Kommentar und Hinweis auf Referenz verlinkt

--- src/main/java/oop/semesterprojekt/enp/OOP_Semesterprojekt/repository/ProduktRepository.java
+++ src/main/java/oop/semesterprojekt/enp/OOP_Semesterprojekt/repository/ProduktRepository.java
@@ -8,6 +8,11 @@ import java.util.List;
 public interface ProduktRepository extends JpaRepository<Produkt, Integer> {
 
     List<Produkt> findByBezeichnung(String bezeichnung);
+
+    /*
+     * Es können beliebige Suchabfragen und Such-/Sortierabfragen formuliert werden:
+     * https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#jpa.query-methods
+     */
     Produkt findByGewicht(double gewicht);
 
 }
